a1593f55e510cd37d6b664ea5ccc3f43a4f38509
Hiroki Osame <hiroki.osame@gmail.com>
2013-04-06 11:45:51 -0400

    new

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+	routes = require('./routes'),
+	user = require('./routes/user'),
+	http = require('http'),
+	path = require('path'),
+	mysql = require('mysql'),
+	geoip = require('geoip-lite');
+
+var connection = mysql.createConnection({
+	host     : '198.74.61.157',
+	user     : 'green',
+	password : 'green',
+});
+
+connection.connect(function(err) {
+	console.log(err);
+});
+
+
+/*
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+*/
 
 var app = express();
 
@@ -10,7 +35,6 @@ var app = express();
 app.set('port', process.env.PORT || 3002);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -23,7 +47,35 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+app.get('/greencaptcha.js', function(req, res){
+	var ip = req.header('x-forwarded-for') || req.ip;
+		ip = "128.197.230.201";
+
+	var geo = geoip.lookup(ip);
+	console.log(geo);
+
+	http.get({
+		host: "explore.data.gov",
+		port: 80,
+		path: "/resource/t6sb-8txz.json?state="+geo.region+"&year=2010",
+	}, function(res) {
+		var page = "";
+		res.setEncoding("utf8");
+		res.on("data", function(data){
+			page += data;
+		}).on("end", function(){
+
+			data = JSON.parse(page);
+			console.log(data);
+		});
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	}).end();
+
+	res.send(geo);
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
